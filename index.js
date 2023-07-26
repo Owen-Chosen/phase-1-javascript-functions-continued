198590b2ddbcb2af6a71367c444bc858c5fa7f75
owen <owen.chosen@gmail.com>
2023-07-27 00:11:07 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun (toDo = "roller-skate") {
+    return `This Saturday, I want to ${toDo}!`;
+} 
+
+function mondayWork (toDo = "go to the office") {
+    return `This Monday, I will ${toDo}.`;
+}
+
+function wrapAdjective (symbol) {
+    if (symbol === "*") {
+        return function (adj = "special") {
+            return `You are ${symbol}${adj}${symbol}!`;
+        }
+    }
+    else if (symbol === "||") {
+        return function (adj = "special") {
+            return `You are ${symbol}${adj}${symbol}!`;
+        } 
+    }
+    
+}
